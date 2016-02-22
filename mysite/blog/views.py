4506163651e61b94714da046f28eb1ebf7983533
lg <leslieg033@gmail.com>
2016-02-22 04:00:27 -0500

    Send emails with Django

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,6 +4,7 @@ from django.core.paginator import Paginator, EmptyPage,\
 								  PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 # Create your views here.
 def post_list(request):
@@ -51,6 +52,7 @@ def post_detail(request, year, month, day, post):
 def post_share(request, post_id):
 	#Retrieve post by id
 	post = get_object_or_404(Post, id=post_id, status='published')
+	sent = False
 
 	if request.method == 'POST':
 		#Form was submitted 
@@ -59,9 +61,15 @@ def post_share(request, post_id):
 			#Form fields passed validation
 			cd = form.cleaned_data
 			#...send email
-		else:
-			form = EmailPostForm()
-		return render(request, 'blog/post/share.html', {'post': post, 'form': form})
+			post_url = request.build_absolute_uri(post.get_absolute_url())
+			subject = '{} ({}) recommends you reading "{}"'.format(cd['name'], cd['email'], post.title)
+			message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(post.title, post_url, cd['name'], cd['comments'])
+			send_mail(subject, message, 'secretary.nsbelehman@gmail.com', [cd['to']])
+			sent = True
+	else:
+		form = EmailPostForm()
+	return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
 
 
 
